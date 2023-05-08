2a4fb545f304e48030308fc34d89dbd230d3aad4
hhimanshu <dev.atwork@yahoo.com>
2023-05-07 21:12:04 -0700

    feat: update paddings

--- src/components/Features/Home/MobileHome.tsx
+++ src/components/Features/Home/MobileHome.tsx
@@ -1,4 +1,4 @@
-import {Card, Icon, Link, Navbar, Page, Tabbar, TabbarLink,} from 'konsta/react';
+import {App, Card, Icon, Link, Navbar, Page, Tabbar, TabbarLink,} from 'konsta/react';
 import {MdDensityMedium, MdEmojiFoodBeverage, MdOutlineFavorite} from "react-icons/md";
 import React, {useState} from "react";
 import {MobilePanel} from "../../Mobile/MobilePanel";
@@ -9,51 +9,55 @@ export default function MobileHome({foods}: { foods: Foods }) {
     const [activeTab, setActiveTab] = useState('tab-1');
 
     return (
-        <Page>
-            <Navbar title="Popular foods" centerTitle left={<Link navbar iconOnly>
-                <Icon
-                    material={<MdDensityMedium onClick={() => setLeftFloatingPanelOpened(true)}/>}
+        <App safeAreas theme="material">
+            <Page>
+                <Navbar title="Popular foods" centerTitle left={<Link navbar iconOnly>
+                    <Icon
+                        material={<MdDensityMedium onClick={() => setLeftFloatingPanelOpened(true)}/>}
+                    />
+
+                </Link>}
                 />
+                <MobilePanel isOpen={leftFloatingPanelOpened} onClick={setLeftFloatingPanelOpened}/>
 
-            </Link>}
-            />
-            <MobilePanel isOpen={leftFloatingPanelOpened} onClick={setLeftFloatingPanelOpened}/>
+                <div className="mb-24">
+                    {foods.map(food => <Card outline>
+                        <p className={`text-md font-medium py-2`}>{food.name}</p>
+                        <p className="text-xs font-normal text-slate-500 uppercase">{food.category}</p>
+                    </Card>)}
+                </div>
 
-            {foods.map(food => <Card outline>
-                <p className={`text-md font-medium py-2`}>{food.name}</p>
-                <p className="text-xs font-normal text-slate-500 uppercase">{food.category}</p>
-            </Card>)}
+                <Tabbar
+                    labels
+                    icons
+                    //labels={isTabbarLabels}
+                    //icons={isTabbarIcons}
+                    className="left-0 bottom-0 fixed"
+                >
+                    <TabbarLink
+                        active={activeTab === 'tab-1'}
+                        onClick={() => setActiveTab('tab-1')}
+                        icon={
 
-            <Tabbar
-                labels
-                icons
-                //labels={isTabbarLabels}
-                //icons={isTabbarIcons}
-                className="left-0 bottom-0 fixed"
-            >
-                <TabbarLink
-                    active={activeTab === 'tab-1'}
-                    onClick={() => setActiveTab('tab-1')}
-                    icon={
+                            <Icon
+                                material={<MdEmojiFoodBeverage className="w-6 h-6"/>}
+                            />
+                        }
+                        label={'Foods'}
+                    />
+                    <TabbarLink
+                        active={activeTab === 'tab-3'}
+                        onClick={() => setActiveTab('tab-3')}
+                        icon={
 
-                        <Icon
-                            material={<MdEmojiFoodBeverage className="w-6 h-6"/>}
-                        />
-                    }
-                    label={'Foods'}
-                />
-                <TabbarLink
-                    active={activeTab === 'tab-3'}
-                    onClick={() => setActiveTab('tab-3')}
-                    icon={
-
-                        <Icon
-                            material={<MdOutlineFavorite className="w-6 h-6"/>}
-                        />
-                    }
-                    label={'Nutrition'}
-                />
-            </Tabbar>
-        </Page>
+                            <Icon
+                                material={<MdOutlineFavorite className="w-6 h-6"/>}
+                            />
+                        }
+                        label={'Nutrition'}
+                    />
+                </Tabbar>
+            </Page>
+        </App>
     );
 }
